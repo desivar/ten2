67583fd52baac1dfe31d20345239cdf5aae8f45a
desivar <jillian_desire@yahoo.es>
2025-05-26 16:36:48 -0600

    add swagger

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// In C:\Users\jilli\ten2\ten2\tennis-api\server.js
+const express = require('express'); // <--- ADD THIS LINE!
 const connectDB = require('./src/config/database');
 const playerRoutes = require('./src/routes/playerRoutes');
 const tournamentRoutes = require('./src/routes/tournamentRoutes');
-const setupSwagger = require('./swagger'); // swagger.js is also at the root
+const setupSwagger = require('./swagger');
 
 const app = express();
 const PORT = process.env.PORT || 5500;
